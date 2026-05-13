f8a911ee6777b7bec1973c317ac27c87861b38ad
sakina <sakinad3@nycstudents.net>
2026-05-13 12:40:11 -0400

    almost finish

---
+++ script.js
@@ -0,0 +1,27 @@
+class MathFunctions {
+
+    constructor(a , b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    add() {
+        return a + b  
+    }
+
+       subtract() {
+            return a - b 
+       }
+
+   multiply() {
+        return a * b 
+   }
+
+      divide () {
+            return a  / b 
+      }
+
+
+      // console.log
+
+}
